9e2f5a368d7bd6a09feaf0987556c42154b9de69
William Parker <wlhh.2001@gmail.com>
2022-12-16 02:24:01 -0500

    Editando la página de teoría

--- src/interfaces/pages/ui_main_pages.py
+++ src/interfaces/pages/ui_main_pages.py
@@ -23,11 +23,11 @@ class Ui_MainPages(object):
     def setupUi(self, MainPages):
         if not MainPages.objectName():
             MainPages.setObjectName(u"MainPages")
-        MainPages.resize(860, 600)
-        self.main_pages_layout = QVBoxLayout(MainPages)
-        self.main_pages_layout.setSpacing(0)
-        self.main_pages_layout.setObjectName(u"main_pages_layout")
-        self.main_pages_layout.setContentsMargins(5, 5, 5, 5)
+        MainPages.resize(864, 620)
+        self.verticalLayout_3 = QVBoxLayout(MainPages)
+        self.verticalLayout_3.setSpacing(0)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.verticalLayout_3.setContentsMargins(5, 5, 5, 5)
         self.pages = QStackedWidget(MainPages)
         self.pages.setObjectName(u"pages")
         self.page_1 = QWidget()
@@ -123,9 +123,124 @@ class Ui_MainPages(object):
         self.pages.addWidget(self.page_1)
         self.page_2 = QWidget()
         self.page_2.setObjectName(u"page_2")
+        self.horizontalLayout_2 = QHBoxLayout(self.page_2)
+        self.horizontalLayout_2.setSpacing(5)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(5, 5, 5, 5)
+        self.teory_col1 = QVBoxLayout()
+        self.teory_col1.setObjectName(u"teory_col1")
+        self.title_section1 = QLabel(self.page_2)
+        self.title_section1.setObjectName(u"title_section1")
+        self.title_section1.setMinimumSize(QSize(0, 60))
+        self.title_section1.setMaximumSize(QSize(16777215, 60))
+        self.title_section1.setFont(font1)
+        self.title_section1.setStyleSheet(u"font: 18pt \"Segoe UI\";")
+
+        self.teory_col1.addWidget(self.title_section1, 0, Qt.AlignHCenter|Qt.AlignTop)
+
+        self.L_line1 = QLabel(self.page_2)
+        self.L_line1.setObjectName(u"L_line1")
+        self.L_line1.setMinimumSize(QSize(276, 2))
+        self.L_line1.setMaximumSize(QSize(16777215, 2))
+
+        self.teory_col1.addWidget(self.L_line1)
+
+        self.label_4 = QLabel(self.page_2)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setMinimumSize(QSize(0, 40))
+        self.label_4.setMaximumSize(QSize(16777215, 40))
+        self.label_4.setStyleSheet(u"font: 14pt \"Segoe UI\";")
+
+        self.teory_col1.addWidget(self.label_4, 0, Qt.AlignHCenter)
+
+        self.mechanism_layout = QHBoxLayout()
+        self.mechanism_layout.setObjectName(u"mechanism_layout")
+
+        self.teory_col1.addLayout(self.mechanism_layout)
+
+        self.grashof_condition = QLabel(self.page_2)
+        self.grashof_condition.setObjectName(u"grashof_condition")
+        self.grashof_condition.setMaximumSize(QSize(16777215, 40))
+        font2 = QFont()
+        font2.setPointSize(14)
+        font2.setBold(False)
+        font2.setItalic(False)
+        self.grashof_condition.setFont(font2)
+        self.grashof_condition.setStyleSheet(u"font: 14pt \"Segoe UI\";")
+
+        self.teory_col1.addWidget(self.grashof_condition, 0, Qt.AlignHCenter|Qt.AlignTop)
+
+        self.ec1_layout = QHBoxLayout()
+        self.ec1_layout.setObjectName(u"ec1_layout")
+
+        self.teory_col1.addLayout(self.ec1_layout)
+
         self.label_2 = QLabel(self.page_2)
         self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(350, 250, 49, 16))
+        self.label_2.setMaximumSize(QSize(16777215, 40))
+        self.label_2.setFont(font2)
+        self.label_2.setStyleSheet(u"font: 14pt \"Segoe UI\";")
+
+        self.teory_col1.addWidget(self.label_2, 0, Qt.AlignHCenter|Qt.AlignTop)
+
+        self.ec2_layout = QHBoxLayout()
+        self.ec2_layout.setObjectName(u"ec2_layout")
+
+        self.teory_col1.addLayout(self.ec2_layout)
+
+
+        self.horizontalLayout_2.addLayout(self.teory_col1)
+
+        self.teory_col2 = QVBoxLayout()
+        self.teory_col2.setObjectName(u"teory_col2")
+        self.title_section2 = QLabel(self.page_2)
+        self.title_section2.setObjectName(u"title_section2")
+        self.title_section2.setMinimumSize(QSize(0, 60))
+        self.title_section2.setMaximumSize(QSize(16777215, 60))
+        self.title_section2.setStyleSheet(u"font: 18pt \"Segoe UI\";")
+
+        self.teory_col2.addWidget(self.title_section2, 0, Qt.AlignHCenter|Qt.AlignTop)
+
+        self.L_line2 = QLabel(self.page_2)
+        self.L_line2.setObjectName(u"L_line2")
+        self.L_line2.setMinimumSize(QSize(276, 0))
+        self.L_line2.setMaximumSize(QSize(16777215, 2))
+
+        self.teory_col2.addWidget(self.L_line2)
+
+        self.ec4_layout = QHBoxLayout()
+        self.ec4_layout.setObjectName(u"ec4_layout")
+
+        self.teory_col2.addLayout(self.ec4_layout)
+
+
+        self.horizontalLayout_2.addLayout(self.teory_col2)
+
+        self.teory_col3 = QVBoxLayout()
+        self.teory_col3.setObjectName(u"teory_col3")
+        self.title_section3 = QLabel(self.page_2)
+        self.title_section3.setObjectName(u"title_section3")
+        self.title_section3.setMinimumSize(QSize(0, 60))
+        self.title_section3.setMaximumSize(QSize(16777215, 60))
+        self.title_section3.setStyleSheet(u"font: 18pt \"Segoe UI\";")
+
+        self.teory_col3.addWidget(self.title_section3, 0, Qt.AlignHCenter|Qt.AlignTop)
+
+        self.L_line3 = QLabel(self.page_2)
+        self.L_line3.setObjectName(u"L_line3")
+        self.L_line3.setMinimumSize(QSize(276, 0))
+        self.L_line3.setMaximumSize(QSize(16777215, 2))
+
+        self.teory_col3.addWidget(self.L_line3)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+
+        self.teory_col3.addLayout(self.horizontalLayout)
+
+
+        self.horizontalLayout_2.addLayout(self.teory_col3)
+
         self.pages.addWidget(self.page_2)
         self.page_3 = QWidget()
         self.page_3.setObjectName(u"page_3")
@@ -142,7 +257,7 @@ class Ui_MainPages(object):
         self.scroll_area.setWidgetResizable(True)
         self.contents = QWidget()
         self.contents.setObjectName(u"contents")
-        self.contents.setGeometry(QRect(0, 0, 840, 580))
+        self.contents.setGeometry(QRect(0, 0, 214, 266))
         self.contents.setStyleSheet(u"background: transparent;")
         self.verticalLayout = QVBoxLayout(self.contents)
         self.verticalLayout.setSpacing(15)
@@ -151,9 +266,9 @@ class Ui_MainPages(object):
         self.title_label = QLabel(self.contents)
         self.title_label.setObjectName(u"title_label")
         self.title_label.setMaximumSize(QSize(16777215, 40))
-        font2 = QFont()
-        font2.setPointSize(16)
-        self.title_label.setFont(font2)
+        font3 = QFont()
+        font3.setPointSize(16)
+        self.title_label.setFont(font3)
         self.title_label.setStyleSheet(u"font-size: 16pt")
         self.title_label.setAlignment(Qt.AlignCenter)
 
@@ -197,12 +312,12 @@ class Ui_MainPages(object):
 
         self.pages.addWidget(self.page_3)
 
-        self.main_pages_layout.addWidget(self.pages)
+        self.verticalLayout_3.addWidget(self.pages)
 
 
         self.retranslateUi(MainPages)
 
-        self.pages.setCurrentIndex(0)
+        self.pages.setCurrentIndex(1)
 
 
         QMetaObject.connectSlotsByName(MainPages)
@@ -214,7 +329,15 @@ class Ui_MainPages(object):
         self.Subtitle.setText(QCoreApplication.translate("MainPages", u"An\u00e1lisis de Posici\u00f3n", None))
         self.label.setText(QCoreApplication.translate("MainPages", u"Empezando", None))
         self.pie.setText("")
-        self.label_2.setText(QCoreApplication.translate("MainPages", u"pagina2", None))
+        self.title_section1.setText(QCoreApplication.translate("MainPages", u"Conceptos Generales", None))
+        self.L_line1.setText("")
+        self.label_4.setText(QCoreApplication.translate("MainPages", u"Mecanismo de 4 barras", None))
+        self.grashof_condition.setText(QCoreApplication.translate("MainPages", u"Condici\u00f3n de Grashof", None))
+        self.label_2.setText(QCoreApplication.translate("MainPages", u"TextLabel", None))
+        self.title_section2.setText(QCoreApplication.translate("MainPages", u"An\u00e1lisis de Posici\u00f3n", None))
+        self.L_line2.setText("")
+        self.title_section3.setText(QCoreApplication.translate("MainPages", u"An\u00e1lisis de Velocidad", None))
+        self.L_line3.setText("")
         self.title_label.setText(QCoreApplication.translate("MainPages", u"Custom Widgets Page", None))
         self.description_label.setText(QCoreApplication.translate("MainPages", u"Here will be all the custom widgets, they will be added over time on this page.\n"
 "I will try to always record a new tutorial when adding a new Widget and updating the project on Patreon before launching on GitHub and GitHub after the public release.", None))
